4138471d3122bc2d1cb185444f9788f9b9fcd63b
Stephen Ribbon <stribbon@icloud.com>
2015-06-04 17:46:31 -0700

    red/black

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your JS goes here
+
+document.onload = function addElement(){
+	for (var i = 0; i < 99; i++){
+		if (i % 2 === 0){
+			var newDiv = document.createElement('div');
+			newDiv.style.backgroundColor = 'red';
+  			newDiv.style.width = '11.1%';
+  			newDiv.style.float = 'left';
+  			newDiv.style.paddingBottom = '11.1%';
+			document.body.appendChild(newDiv);
+		}else {
+			var newDiv = document.createElement('div');
+			newDiv.style.backgroundColor = 'black';
+  			newDiv.style.width = '11.1%';
+  			newDiv.style.float = 'left';
+  			newDiv.style.paddingBottom = '11.1%';
+			document.body.appendChild(newDiv);
+		}	
+	}
+}();
